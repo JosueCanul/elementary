7d153868e4560dbb685cba8d464b2bad1f27be1e
Josue <josuecanul2605@gmail.com>
2025-05-24 20:59:00 -0600

    restore version

--- target/generated-sources/annotations/com/elementary/mx/elementary/mapper/ScoreMapperImpl.java
+++ target/generated-sources/annotations/com/elementary/mx/elementary/mapper/ScoreMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-18T16:15:59-0600",
+    date = "2025-05-24T20:57:24-0600",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
